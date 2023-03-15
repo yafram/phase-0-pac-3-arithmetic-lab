b737984c990b84e80eb6b98068eda32fcb6b47ad
Jacqueline W Sinclair <faycello75@gmail.com>
2023-03-15 00:13:11 -0400

    coded basic math operations as well as for parseInt and parseFloat

---
+++ index.js
@@ -0,0 +1,36 @@
+function add(a, b) {
+
+    return a  + b;
+}
+add(8, 9)
+
+function subtract(a, b) {
+    return a - b;    
+}
+subtract()
+
+function multiply(a, b) {
+    return a * b;    
+}
+function divide(a, b) {
+    return a / b;
+}
+function increment(n) {   
+    return n + 1;    
+}
+function decrement(n) {
+    return n - 1;   
+}
+
+
+function makeInt(n) {
+    return(parseInt(n, '10'));
+    
+}
+
+function preserveDecimal(n) {
+    return parseFloat(".n");
+}
+function preserveDecimal(n) {
+    return parseFloat(n);
+}
